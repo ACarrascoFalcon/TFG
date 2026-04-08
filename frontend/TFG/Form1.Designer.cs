cad1c0db241405922df417ba5aa8d4a9a942b7cd
Alex <alejandro.2006.ca@gmail.com>
2026-04-08 13:06:22 +0200

    Inicio sesion

--- frontend/TFG/Form1.Designer.cs
+++ frontend/TFG/Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.nombre = new System.Windows.Forms.TextBox();
             this.apellido = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // nombre
@@ -50,30 +49,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(75, 207);
+            this.button1.Location = new System.Drawing.Point(402, 211);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(108, 40);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Aceptar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(190, 207);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(571, 278);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.apellido);
             this.Controls.Add(this.nombre);
@@ -89,6 +77,5 @@
         private System.Windows.Forms.TextBox nombre;
         private System.Windows.Forms.TextBox apellido;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
